87f9af3b241d217f94f8c736ebdb6ea34c506dc8
Michael Teye Yaw Dapaah <michaeldapaah0@gmail.com>
2025-02-16 22:21:10 +0000

    working on components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,7 +1,7 @@
 import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import MainComponent from './components/MainContent'
+import MainContent from './components/MainContent'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -15,7 +15,7 @@ function App() {
       <div>
         <WelcomeMessage />
         <Header />
-        <MainComponent />
+        <MainContent />
         <Footer />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
