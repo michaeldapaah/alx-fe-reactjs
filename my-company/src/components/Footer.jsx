5b2a42a8b969b281d5c8e3c0452b58c230f8d7b1
Michael Teye Yaw Dapaah <michaeldapaah0@gmail.com>
2025-02-23 23:21:20 +0000

    my company website

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+      <footer style={{ textAlign: 'center', padding: '20px', background: '#eee' }}>
+        <p>© 2024 My Company. All Rights Reserved.</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
