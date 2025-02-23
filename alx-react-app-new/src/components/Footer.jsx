edae14d6cd07d0e32e201ffb2d7f0d0246827edb
Michael Teye Yaw Dapaah <michaeldapaah0@gmail.com>
2025-02-23 20:57:38 +0000

    working with styling on react

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+    <>
+        <footer style={{ backgroundColor: 'black', color: 'white', textAlign: 'center', padding: '10px' }}>
+           <p>© 2023 City Lovers</p>
+        </footer>
+    </>
+    );
+};
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+const MainContent = () => {
+    return (
+    <>
+        <main style={{ padding: '20px', fontSize: '18px', lineHeight: '1.6' }}>
+            <p style={{ color: 'darklategray' }}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    </>
+    );
+};
+
+export default MainContent;
