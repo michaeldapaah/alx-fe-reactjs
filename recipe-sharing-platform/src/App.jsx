ec62f72d7554f6de990a469b4745689e3ed85be5
Michael Teye Yaw Dapaah <michaeldapaah0@gmail.com>
2025-03-17 15:26:55 +0000

    initial page for the recipe sharing platform

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,13 @@
+import UserProfile from "./components/UserProfile"
+
+function App() {
+
+
+  return (
+    <div class="text-blue-50">
+        <UserProfile />
+    </div>
+  )
+}
+
+export default App
---
+++ recipe-sharing-platform/src/components/UserProfile.jsx
@@ -0,0 +1,9 @@
+function UserProfile(){
+    return (
+        <div>
+            <button class="text-blue-500">click me</button>
+        </div>
+    )
+}
+
+export default UserProfile;
