ead01433156dc1c4d97b1cee5350c957fa202907
Michael Teye Yaw Dapaah <michaeldapaah0@gmail.com>
2025-03-30 11:26:07 +0000

    feat: adding search characteristics

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,6 +1,6 @@
 
 import { useState } from "react";
-import SearchBar from "./components/SearchBar";
+import SearchBar from "./components/Search";
 import UserCard from "./components/UserCard";
 import fetchUserData from "./services/githubService";
 
